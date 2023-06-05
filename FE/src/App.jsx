05b9553408ba04d0d22fd4aeaca470e53c509847
SonNB <ngsn176@gmail.com>
2023-06-05 15:57:02 +0700

    Create SearchForm

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import SearchForm from './components/SearchForm/SearchForm';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const SearchFormDemo = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="SearchFormDemo">
+      <h1>Search for Teachers and Courses</h1>
+      <SearchForm />
+    </div>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <div className="App">
+      {/* Đây là nội dung gốc của App */}
+      <h1>VietnameseTutor</h1>
+      {/* ... */}
+      <SearchFormDemo /> {/* Thêm component TeacherSearchDemo */}
+      {/* ... */}
+    </div>
+  );
+};
+
+export default App;
